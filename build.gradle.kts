85a322e162b315566569630b2cdc5873fe46f90e
Julian Schmitt <64885645+juschmitt-ioki@users.noreply.github.com>
2025-01-13 10:22:01 +0100

    Prepare new release

--- build.gradle.kts
+++ build.gradle.kts
@@ -47,7 +47,7 @@ publishing {
 
             groupId = "com.ioki.result"
             artifactId = artifactId.lowercase()
-            version = "0.3.0-SNAPSHOT"
+            version = "0.3.0"
 
             pom {
                 name.set("Result")
